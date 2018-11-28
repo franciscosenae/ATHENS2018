f496ebc457a9f545007d2890993cb98179928ab0
Nathanael Bosch <nathanael.bosch@tum.de>
2018-11-28 09:39:47 +0100

    Renamed and modified `visualization.py`, now `evaluation.py`

--- evaluation.py
+++ evaluation.py
@@ -10,42 +10,6 @@ from sklearn import svm
 from sklearn.metrics import roc_curve
 from sklearn.model_selection import train_test_split
 
-def _make_meshgrid(x, y, h=.02):
-    """Create a mesh of points to plot in
-
-    Parameters
-    ----------
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
-
-    Returns
-    -------
-    xx, yy : ndarray
-    """
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    return xx, yy
-
-
-def _plot_contours(ax, clf, xx, yy, **params):
-    """Plot the decision boundaries for a classifier.
-
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    xx: meshgrid ndarray
-    yy: meshgrid ndarray
-    params: dictionary of params to pass to contourf, optional
-    """
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = 1-Z.reshape(xx.shape)
-    out = ax.contourf(xx, yy, Z, **params)
-    return out
-
 
 def plot_contours(X, y, clf, ax=None):
     if ax is None:
@@ -53,11 +17,22 @@ def plot_contours(X, y, clf, ax=None):
         ax, = fig.get_axes()
 
     X0, X1 = X[X.columns[0]], X[X.columns[1]]
-    xx, yy = _make_meshgrid(X0, X1)
 
-    _plot_contours(ax, clf, xx, yy,
-                   cmap=plt.cm.coolwarm, alpha=0.4)
+    # Make meshgrid
+    h = .02
+    x_min, x_max = X0.min() - 1, X0.max() + 1
+    y_min, y_max = X1.min() - 1, X1.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # Plot contours
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = 1-Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.4)
+
+    # Plot points
     ax.scatter(X0, X1, c=1-y, cmap=plt.cm.coolwarm, s=100, edgecolors='k')
+
     ax.set_xlabel(X.columns[0])
     ax.set_ylabel(X.columns[1])
     return ax
@@ -70,6 +45,7 @@ def plot_roc(X, y, clf):
 
 
 def plot_confusion_matrix(y_true, y_predicted):
+    """Plots both the absolute and relative confusion matrix"""
     confusion_matrix = sk.metrics.confusion_matrix(
         y_true, y_predicted)
     # print(confusion_matrix)
@@ -94,8 +70,11 @@ def plot_confusion_matrix(y_true, y_predicted):
 
 
 def results_on_years(clf, FEATURES_USED, years=[2016, 2017]):
+    """Wrapper to present the results on the 2016 and 2017 datasets"""
+
     if len(FEATURES_USED) == 2:
         fig, sub = plt.subplots(1, 2)
+
     for i, year in enumerate(years):
 
         _df = pd.read_csv(f'data/processed/{year}.csv')
@@ -117,35 +96,5 @@ def print_metrics(y_true, y_predicted):
         'Accuracy': sk.metrics.accuracy_score(y_true, y_predicted),
         'Precision': sk.metrics.precision_score(y_true, y_predicted),
         'Recall': sk.metrics.recall_score(y_true, y_predicted)
-}
+    }
     pprint(metrics)
-
-
-def main():
-    df = pd.read_csv('data/processed/2018.csv')
-
-    X = df[['std', 'gy']]
-    y = df.label == 'brushing'
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5,
-
-                                                        random_state=0)
-
-    clf = svm.SVC(kernel='linear')
-    # clf = svm.SVC(gamma='scale', kernel='rbf')
-    clf.fit(X_train, y_train)
-
-    fig, sub = plt.subplots(1, 1)
-    ax, = fig.get_axes()
-
-    X0, X1 = X[X.columns[0]], X[X.columns[1]]
-    xx, yy = make_meshgrid(X0, X1)
-    plot_contours(ax, clf, xx, yy,
-                  cmap=plt.cm.coolwarm, alpha=0.8)
-    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-    plt.show()
-
-
-
-if __name__ == '__main__':
-    main()
